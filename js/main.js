12b320c2e4e216eafc67173341c37d6d8198f438
acho <achoegbunu@gmail.com>
2018-09-01 23:26:40 -0700

    made some changes to the html and finished working on the javascript

--- js/main.js
+++ js/main.js
@@ -1,5 +1,46 @@
 var user = {
 	name: "Acho Egbunu",
-	image: "",
+	image: "img/Acho.JPG",
 	progress: "0.69"
 }
+var courseArray = [
+{ 
+	title: "Introduction to FIFA",
+    subject: "Gaming"
+}, 
+{
+	title: "Eating Healthy",
+    subject: "Health Education"
+},
+{
+	title: "Self Driving Vehicles",
+	subject: "technology"
+}];
+
+var activityArray = [
+{
+	title: "Completed Week 13 Exercises",
+	subject: "Coding 305"
+},
+{
+	title: "Finished Playing the Soccer Season",
+	subject: "Soccer Talk"
+},
+{
+	title: "The Art of Appreciating Beautiful Ladies",
+	subject: "Good Eyes"
+}];
+
+var accountArray = [
+{
+	title: "Enrolled for Classes",
+	subject: "Codify Academy"
+},
+{
+	title: "Enrollment Status",
+	subject: "Active Student"
+},
+{
+	title: "Coding Struggles",
+	subject: "Student Thoughts"
+}];
